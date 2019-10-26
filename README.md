2c6e55f3c6ccded44f53980672a411f6753c3130
tim-jeffries <timothyrexjeffries@gmail.com>
2019-10-26 10:05:10 -0700

    Create-react-app & initial scaffolding

--- README.md
+++ README.md
@@ -1,1 +1,30 @@
 # tim-front-end
+
+/*
+
+Seeker btn
+Company btn
+
+Seeker login (no function)
+
+Company Login (onClick: redirect to company login form)
+
+Company login form
+ GET backend  (if else)
+    usename field
+    Password field
+    Login btn
+
+
+  **Successful Login**
+
+  Company profile
+    Hello, Company.name
+      location
+      description
+      ...get stuff
+
+    jobs btn (no functionality)
+    seeker btn (no functionality)
+
+*/
---
+++ droom-app/src/App.js
@@ -0,0 +1,56 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
+
+
+/*
+
+Seeker btn
+Company btn
+
+Seeker login (no function)
+
+Company Login (onClick: redirect to company login form)
+
+Company login form
+ GET backend  (if else)
+    usename field
+    Password field
+    Login btn
+
+
+  **Successful Login**
+
+  Company profile
+    Hello, Company.name
+      location
+      description
+      ...get stuff
+
+    jobs btn (no functionality)
+    seeker btn (no functionality)
+
+*/
